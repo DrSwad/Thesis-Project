67dab3997ec4d2978d64d6b74a7d1bf7ed4f5207
Nayeemul Islam Swad <swad.mailbox@gmail.com>
2023-12-29 19:51:36 +0600

    Remove unnecessary threshold related properties and methods

--- Parameters/Variable.py
+++ Parameters/Variable.py
@@ -1,18 +1,5 @@
 class Variable:
-    local_threshold = 0.0
-    window_size = 0.0
     mu = 0.0
     WAM = 0.0
     size_of_dataset: int = 0
-    size_of_increment: int = 0
     eps = 0.000000001
-
-    @staticmethod
-    def __init__(
-        local_threshold: float, window_size: float, mu: float, wam: float
-    ) -> None:
-        Variable.local_threshold = local_threshold
-        Variable.window_size = window_size
-        Variable.mu = mu
-        Variable.WAM = wam
-        pass
--- UtilityTechniques/ThresholdCalculation.py
+++ UtilityTechniques/ThresholdCalculation.py
@@ -4,32 +4,6 @@ from Parameters.Variable import Variable
 
 class ThresholdCalculation:
     @staticmethod
-    def get_minExpSup() -> float:
-        return UserDefined.min_sup * Variable.size_of_dataset
-
-    @staticmethod
-    def get_semi_threshold() -> float:
-        return ThresholdCalculation.get_minExpSup() * Variable.mu
-
-    @staticmethod
-    def get_local_threshold() -> float:
-        return (
-            UserDefined.min_sup
-            * (
-                Variable.window_size * Variable.size_of_increment
-                + Variable.size_of_dataset
-            )
-        ) / (Variable.window_size * Variable.size_of_increment)
-
-    @staticmethod
-    def get_minExpWgtSup() -> float:
-        return Variable.WAM * Variable.size_of_dataset * UserDefined.wgt_factor
-
-    @staticmethod
-    def update_dataset(sz: int) -> None:
-        Variable.size_of_dataset += sz
-
-    @staticmethod
     def get_wgt_exp_sup() -> float:
         return (
             UserDefined.min_sup
